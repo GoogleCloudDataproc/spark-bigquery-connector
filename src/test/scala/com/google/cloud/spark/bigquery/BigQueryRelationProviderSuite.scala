3371e8cde8a9f9cfac808fecb072e7c9e826842c
Jiri Syrovy <syrovy.jiri@gmail.com>
2019-06-25 22:36:45 +0200

    #6 allow specifying auth via Spark Configuration (#20)
    * #6 allow specifying auth via Spark Configuration
    
    * #6 allow specifying auth via Spark Configuration
    
    * #6 allow credentials to be provided in configuration and not only in parameters
    
    * #6 updated readme
    
    * add option to specify service account json key file
    
    * fix
    
    * add option to specify credentials through file.
    
    * tweak readme to be more precise.
    
    * ensure option names are case sensitive; add todo regarding auth-option test case; better exception message.

--- src/test/scala/com/google/cloud/spark/bigquery/BigQueryRelationProviderSuite.scala
+++ src/test/scala/com/google/cloud/spark/bigquery/BigQueryRelationProviderSuite.scala
@@ -26,17 +26,22 @@ import org.mockito.{Matchers, Mock, MockitoAnnotations}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
+class BigQueryRelationProviderSuite
+    extends FunSuite
+    with BeforeAndAfter
+    with MockitoSugar {
 
-class BigQueryRelationProviderSuite extends FunSuite with BeforeAndAfter with MockitoSugar {
+  // TODO(#23) Add test case covering 'credentials' and 'credentialsFile' options
 
   private val ID = TableId.of("test_project", "test_dataset", "test_table")
   private val TABLE_NAME = "test_project:test_dataset.test_table"
   private val TABLE = TableInfo.of(
     ID,
-    StandardTableDefinition.newBuilder()
-        .setSchema(Schema.of(Field.of("foo", LegacySQLTypeName.STRING)))
-        .setNumBytes(42L)
-        .build())
+    StandardTableDefinition
+      .newBuilder()
+      .setSchema(Schema.of(Field.of("foo", LegacySQLTypeName.STRING)))
+      .setNumBytes(42L)
+      .build())
 
   @Mock
   private var sqlCtx: SQLContext = _
@@ -53,11 +58,13 @@ class BigQueryRelationProviderSuite extends FunSuite with BeforeAndAfter with Mo
   before {
     MockitoAnnotations.initMocks(this)
     conf = new Configuration(false)
-    provider = new BigQueryRelationProvider(() => bigQuery)
+    provider =
+      new BigQueryRelationProvider(() => Some(bigQuery), Some(ID.getProject))
     table = TestUtils.table(TABLE)
 
     when(sqlCtx.sparkContext).thenReturn(sc)
     when(sc.hadoopConfiguration).thenReturn(conf)
+    when(sqlCtx.getAllConfs).thenReturn(Map.empty[String, String])
   }
 
   after {
@@ -84,4 +91,3 @@ class BigQueryRelationProviderSuite extends FunSuite with BeforeAndAfter with Mo
     verify(bigQuery).getTable(Matchers.eq(ID))
   }
 }
-
