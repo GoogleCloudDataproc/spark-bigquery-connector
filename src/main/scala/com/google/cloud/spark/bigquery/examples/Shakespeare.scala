1f5022fdddd3e643d62a13796f7b561c652fefb7
David Rabinowitz <davidrab@gmail.com>
2019-10-11 12:47:18 -0700

    updated example

--- src/main/scala/com/google/cloud/spark/bigquery/examples/Shakespeare.scala
+++ src/main/scala/com/google/cloud/spark/bigquery/examples/Shakespeare.scala
@@ -15,23 +15,32 @@
  */
 package com.google.cloud.spark.bigquery.examples
 
-import java.nio.file.Files
-
 import com.google.cloud.spark.bigquery._
-import com.typesafe.scalalogging.Logger
 import org.apache.spark.sql.SparkSession
 
 object Shakespeare {
-  private val log = Logger(getClass)
   def main(args: Array[String]) {
-    val spark = SparkSession.builder().appName("test").getOrCreate()
-    val sc = spark.sparkContext
+    val spark = SparkSession.builder()
+      .appName("spark-bigquery-demo")
+      .getOrCreate()
 
-    var df = spark.read.bigquery("publicdata.samples.shakespeare").cache()
-    df.show()
-    df.printSchema()
-    val path = Files.createTempDirectory("spark-bigquery").resolve("out")
-    log.warn("Writing table out to {}", path)
-    df.write.csv(path.toString)
+    // Use the Cloud Storage bucket for temporary BigQuery export data used
+    // by the connector. This assumes the Cloud Storage connector for
+    // Hadoop is configured.r
+    val bucket = spark.sparkContext.hadoopConfiguration.get("fs.gs.system.bucket")
+    spark.conf.set("temporaryGcsBucket", bucket)
+
+    // Load data in from BigQuery.
+    val wordsDF = spark.read.bigquery("publicdata.samples.shakespeare").cache()
+    wordsDF.show()
+    wordsDF.printSchema()
+    wordsDF.createOrReplaceTempView("words")
+
+    // Perform word count.
+     val wordCountDF = spark.sql(
+      "SELECT word, SUM(word_count) AS word_count FROM words GROUP BY word")
+
+    // Saving the data to BigQuery
+    wordCountDF.write.bigquery("wordcount_dataset.wordcount_output")
   }
 }
