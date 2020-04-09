12c93d21f033da8139ba84d793dedaa3ff5cca3c
David Rabinowitz <davidrabinowitz@users.noreply.github.com>
2020-04-09 14:52:56 -0700

    Datasource v2 initial work, not working yet (#153)

---
+++ connector/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryErrorCode.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigquery.connector.common;
+
+public enum BigQueryErrorCode {
+    BIGQUERY_VIEW_DESTINATION_TABLE_CREATION_FAILED(0),
+    BIGQUERY_DATETIME_PARSING_ERROR(1),
+    BIGQUERY_FAILED_TO_EXECUTE_QUERY(2),
+    // Should be last
+    UNSUPPORTED(9998),
+    UNKNOWN(9999);
+
+    final int code;
+
+    BigQueryErrorCode(int code) {
+        this.code = code;
+    }
+
+
+    public int getCode() {
+        return code;
+    }
+
+}
---
+++ connector/src/main/java/com/google/cloud/spark/bigquery/BigQueryInputPartitionReader.java
@@ -0,0 +1,45 @@
+package com.google.cloud.spark.bigquery;
+
+import com.google.cloud.bigquery.storage.v1beta1.BigQueryStorageClient;
+import com.google.cloud.bigquery.storage.v1beta1.Storage;
+import com.google.common.collect.Iterators;
+import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.sources.v2.reader.InputPartitionReader;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+class BigQueryInputPartitionReader implements InputPartitionReader<InternalRow> {
+
+    private BigQueryStorageClient bigQueryStorageClient;
+    private Iterator<InternalRow> internalRowIterator;
+    private InternalRow currentRow;
+
+
+    BigQueryInputPartitionReader(
+            Iterator<Storage.ReadRowsResponse> rows,
+            ReadRowsResponseToInternalRowIteratorConverter converter,
+            BigQueryStorageClient bigQueryStorageClient) {
+        this.internalRowIterator = Iterators.concat(Iterators.transform(rows, converter::convert));
+        this.bigQueryStorageClient = bigQueryStorageClient;
+    }
+
+    @Override
+    public boolean next() throws IOException {
+        if (internalRowIterator.hasNext()) {
+            currentRow = internalRowIterator.next();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public InternalRow get() {
+        return currentRow;
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+}
