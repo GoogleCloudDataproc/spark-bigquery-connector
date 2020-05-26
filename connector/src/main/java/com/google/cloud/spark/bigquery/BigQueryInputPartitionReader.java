8e02346e93d0f1f8255c9cfbd9704349bc848774
David Rabinowitz <davidrab@google.com>
2020-05-26 08:53:54 -0700

    Format fix and adding missing license header

--- connector/src/main/java/com/google/cloud/spark/bigquery/BigQueryInputPartitionReader.java
+++ connector/src/main/java/com/google/cloud/spark/bigquery/BigQueryInputPartitionReader.java
@@ -1,14 +1,26 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.spark.bigquery;
 
-import com.google.cloud.bigquery.storage.v1beta1.BigQueryStorageClient;
 import com.google.cloud.bigquery.storage.v1beta1.Storage;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterators;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.sources.v2.reader.InputPartitionReader;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Iterator;
 
 class BigQueryInputPartitionReader implements InputPartitionReader<InternalRow> {
@@ -27,8 +39,8 @@ class BigQueryInputPartitionReader implements InputPartitionReader<InternalRow> 
 
     @Override
     public boolean next() throws IOException {
-        while(!rows.hasNext()) {
-            if(!readRowsResponses.hasNext()) {
+        while (!rows.hasNext()) {
+            if (!readRowsResponses.hasNext()) {
                 return false;
             }
             Storage.ReadRowsResponse readRowsResponse = readRowsResponses.next();
