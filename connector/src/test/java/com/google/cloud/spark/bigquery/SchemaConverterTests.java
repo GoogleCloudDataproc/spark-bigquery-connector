3c57a486ab8aa74ca97c9d21b6d6efab24505244
Yuval Medina <ymed@google.com>
2020-06-22 23:37:50 +0000

    Spark->BQ Schema working correctly. Blocked out Map functionality, as it is not supported. Made SchemaConverters, Schema-unit-tests more readable. Improved use of BigQuery library functions/iteration in SchemaConverters

--- connector/src/test/java/com/google/cloud/spark/bigquery/SchemaConverterTests.java
+++ connector/src/test/java/com/google/cloud/spark/bigquery/SchemaConverterTests.java
@@ -8,12 +8,165 @@ import com.google.cloud.bigquery.storage.v1alpha2.ProtoBufProto;
 import com.google.cloud.spark.bigquery.v2.YuvalSchemaConverters;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.spark.sql.types.*;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.*;
 
 public class SchemaConverterTests {
+
+    // TODO: logger here
+    private final Logger logger = LogManager.getLogger("com.google.cloud.spark");
+
+    // TODO: test for makeBQColumn
+
+    @Test
+    public void testToBQSchema1IntegerColumn() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        StructType schema = Spark1IntegerColumn;
+        Schema check = BQ1IntegerColumn;
+
+        Schema converted = YuvalSchemaConverters.toBigQuerySchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    @Test
+    public void testToBQSchema2Columns() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        StructType schema = Spark2Columns;
+        Schema check = BQ2Columns;
+
+        Schema converted = YuvalSchemaConverters.toBigQuerySchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    @Test
+    public void testToBQSchemaNestedStruct() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        StructType schema = SparkNestedStruct;
+        Schema check = BQNestedStruct;
+
+        Schema converted = YuvalSchemaConverters.toBigQuerySchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString()+"\n"+
+                converted.getFields().get(0).getSubFields().toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    @Test
+    public void testToBQSchemaArray() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        StructType schema = SparkArray;
+        Schema check = BQArray;
+
+        Schema converted = YuvalSchemaConverters.toBigQuerySchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    /*
+    @Test
+    public void testToBQSchemaMap() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        StructType schema = SparkMap;
+        Schema check = BQMap;
+
+        Schema converted = YuvalSchemaConverters.toBigQuerySchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString()+"\n"+
+                converted.getFields().get(0).getSubFields().toString()+"\n"+
+                converted.getFields().get(0).getSubFields().get(0).getSubFields().toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+     */
+
+    @Test
+    public void testToProtoSchema1IntegerColumn() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        Schema schema = BQ1IntegerColumn;
+        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
+
+        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    @Test
+    public void testToProtoSchema2Columns() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        Schema schema = BQ2Columns;
+        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
+
+        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    @Test
+    public void testToProtoSchemaNestedStruct() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        Schema schema = BQNestedStruct;
+        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
+
+        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    @Test
+    public void testToProtoSchemaArray() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        Schema schema = BQArray;
+        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
+
+        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+
+    /*
+    @Test
+    public void testToProtoSchemaMap() throws Exception {
+        logger.setLevel(Level.DEBUG);
+
+        Schema schema = BQMap;
+        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
+
+        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
+
+        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
+
+        assertThat(converted).isEqualTo(check);
+    }
+     */
 
     public final StructType Spark1IntegerColumn = new StructType()
             .add(new StructField("Numbers", DataTypes.IntegerType, true, null));
@@ -25,9 +178,9 @@ public class SchemaConverterTests {
             .add(new StructField("Strings", DataTypes.StringType,
                     false, null));
     public final Schema BQ2Columns = Schema.of(Field.newBuilder("Numbers", LegacySQLTypeName.INTEGER, (FieldList) null)
-            .setMode(Field.Mode.NULLABLE).build(),
-                Field.newBuilder("Strings", LegacySQLTypeName.STRING, (FieldList) null)
-            .setMode(Field.Mode.REQUIRED).build());
+                    .setMode(Field.Mode.NULLABLE).build(),
+            Field.newBuilder("Strings", LegacySQLTypeName.STRING, (FieldList) null)
+                    .setMode(Field.Mode.REQUIRED).build());
 
     public final StructType SparkNestedStruct = new StructType().add(new StructField("Struct",
             DataTypes.createStructType(new StructField[]{new StructField("Number", DataTypes.IntegerType,
@@ -45,165 +198,18 @@ public class SchemaConverterTests {
     public final StructType SparkArray = new StructType().add(new StructField("Array",
             DataTypes.createArrayType(DataTypes.IntegerType),
             true, null));
-    public final Schema BQArray = Schema.of(Field.newBuilder("Array", LegacySQLTypeName.RECORD,
-            Field.newBuilder("", LegacySQLTypeName.INTEGER, (FieldList) null)
-                    .setMode(Field.Mode.REPEATED).build())
-            .setMode(Field.Mode.NULLABLE).build());
+    public final Schema BQArray = Schema.of(Field.newBuilder("Array", LegacySQLTypeName.INTEGER, (FieldList) null)
+            .setMode(Field.Mode.REPEATED).build());
 
     public final StructType SparkMap = new StructType().add(new StructField("Map",
             DataTypes.createMapType(DataTypes.IntegerType, DataTypes.StringType),
             true, null));
     public final Schema BQMap = Schema.of(Field.newBuilder("Map", LegacySQLTypeName.RECORD,
-            Field.newBuilder("Pair", LegacySQLTypeName.RECORD, FieldList.of(
+            Field.newBuilder("Pair", LegacySQLTypeName.RECORD, FieldList.of(  // TODO: map - pair, k, v names?
                     Field.newBuilder("K", LegacySQLTypeName.INTEGER, (FieldList) null)
                             .setMode(Field.Mode.NULLABLE).build(),
                     Field.newBuilder("V", LegacySQLTypeName.STRING, (FieldList) null)
                             .setMode(Field.Mode.NULLABLE).build()
             )).setMode(Field.Mode.REPEATED).build())
             .setMode(Field.Mode.NULLABLE).build());
-
-    @Test
-    public void testToBQSchema1IntegerColumn() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        StructType schema = Spark1IntegerColumn;
-        Schema check = BQ1IntegerColumn;
-
-        Schema converted = YuvalSchemaConverters.toBQSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToBQSchema2Columns() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        StructType schema = Spark2Columns;
-        Schema check = BQ2Columns;
-
-        Schema converted = YuvalSchemaConverters.toBQSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToBQSchemaNestedStruct() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        StructType schema = SparkNestedStruct;
-        Schema check = BQNestedStruct;
-
-        Schema converted = YuvalSchemaConverters.toBQSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString()+"\n"+
-                converted.getFields().get(0).getSubFields().toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToBQSchemaArray() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        StructType schema = SparkArray;
-        Schema check = BQArray;
-
-        Schema converted = YuvalSchemaConverters.toBQSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString()+"\n"+
-                converted.getFields().get(0).getSubFields().toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToBQSchemaMap() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        StructType schema = SparkMap;
-        Schema check = BQMap;
-
-        Schema converted = YuvalSchemaConverters.toBQSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString()+"\n"+
-                converted.getFields().get(0).getSubFields().toString()+"\n"+
-                converted.getFields().get(0).getSubFields().get(0).getSubFields().toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToProtoSchema1IntegerColumn() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        Schema schema = BQ1IntegerColumn;
-        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
-
-        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToProtoSchema2Columns() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        Schema schema = BQ2Columns;
-        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
-
-        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToProtoSchemaNestedStruct() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        Schema schema = BQNestedStruct;
-        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
-
-        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToProtoSchemaArray() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        Schema schema = BQArray;
-        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
-
-        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
-    @Test
-    public void testToProtoSchemaMap() throws Exception {
-        LogManager.getLogger("com.google.cloud.spark").setLevel(Level.INFO);
-
-        Schema schema = BQMap;
-        ProtoBufProto.ProtoSchema check = null; // TODO: make .proto file for this test
-
-        ProtoBufProto.ProtoSchema converted = YuvalSchemaConverters.toProtoSchema(schema);
-
-        LogManager.getLogger("com.google.cloud.spark").info(converted.toString());
-
-        assertThat(converted).isEqualTo(check);
-    }
-
 }
