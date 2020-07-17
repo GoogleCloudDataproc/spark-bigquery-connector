a7455f37900732806dec8dfd544f38d71db8ddef
David Rabinowitz <davidrab@gmail.com>
2020-07-17 08:12:29 -0700

    Applying google-java-format, no code change

--- connector/src/test/java/com/google/cloud/bigquery/connector/common/ReadRowsHelperTest.java
+++ connector/src/test/java/com/google/cloud/bigquery/connector/common/ReadRowsHelperTest.java
@@ -15,7 +15,6 @@
  */
 package com.google.cloud.bigquery.connector.common;
 
-
 import com.google.cloud.bigquery.storage.v1.ReadRowsRequest;
 import com.google.cloud.bigquery.storage.v1.ReadRowsResponse;
 import com.google.common.collect.ImmutableList;
@@ -28,61 +27,60 @@ import java.util.Iterator;
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.mock;
 
-public class ReadRowsHelperTest
-{
-    // it is not used, we just need the reference
-    BigQueryReadClientFactory clientFactory = mock(BigQueryReadClientFactory.class);
-    private ReadRowsRequest.Builder request = ReadRowsRequest.newBuilder().setReadStream("test");
+public class ReadRowsHelperTest {
+  // it is not used, we just need the reference
+  BigQueryReadClientFactory clientFactory = mock(BigQueryReadClientFactory.class);
+  private ReadRowsRequest.Builder request = ReadRowsRequest.newBuilder().setReadStream("test");
 
-    @Test
-    public void testNoFailures()
-    {
-        MockResponsesBatch batch1 = new MockResponsesBatch();
-        batch1.addResponse(ReadRowsResponse.newBuilder().setRowCount(10).build());
-        batch1.addResponse(ReadRowsResponse.newBuilder().setRowCount(11).build());
+  @Test
+  public void testNoFailures() {
+    MockResponsesBatch batch1 = new MockResponsesBatch();
+    batch1.addResponse(ReadRowsResponse.newBuilder().setRowCount(10).build());
+    batch1.addResponse(ReadRowsResponse.newBuilder().setRowCount(11).build());
 
-        // so we can run multiple tests
-        ImmutableList<ReadRowsResponse> responses = ImmutableList.copyOf(
-                new MockReadRowsHelper(clientFactory, request, 3, ImmutableList.of(batch1))
-                        .readRows());
+    // so we can run multiple tests
+    ImmutableList<ReadRowsResponse> responses =
+        ImmutableList.copyOf(
+            new MockReadRowsHelper(clientFactory, request, 3, ImmutableList.of(batch1)).readRows());
 
-        assertThat(responses.size()).isEqualTo(2);
-        assertThat(responses.stream().mapToLong(ReadRowsResponse::getRowCount).sum()).isEqualTo(21);
+    assertThat(responses.size()).isEqualTo(2);
+    assertThat(responses.stream().mapToLong(ReadRowsResponse::getRowCount).sum()).isEqualTo(21);
+  }
+
+  @Test
+  public void testRetryOfSingleFailure() {
+    MockResponsesBatch batch1 = new MockResponsesBatch();
+    batch1.addResponse(ReadRowsResponse.newBuilder().setRowCount(10).build());
+    batch1.addException(
+        new StatusRuntimeException(
+            Status.INTERNAL.withDescription("Received unexpected EOS on DATA frame from server.")));
+    MockResponsesBatch batch2 = new MockResponsesBatch();
+    batch2.addResponse(ReadRowsResponse.newBuilder().setRowCount(11).build());
+
+    ImmutableList<ReadRowsResponse> responses =
+        ImmutableList.copyOf(
+            new MockReadRowsHelper(clientFactory, request, 3, ImmutableList.of(batch1, batch2))
+                .readRows());
+
+    assertThat(responses.size()).isEqualTo(2);
+    assertThat(responses.stream().mapToLong(ReadRowsResponse::getRowCount).sum()).isEqualTo(21);
+  }
+
+  private static final class MockReadRowsHelper extends ReadRowsHelper {
+    Iterator<MockResponsesBatch> responses;
+
+    MockReadRowsHelper(
+        BigQueryReadClientFactory clientFactory,
+        ReadRowsRequest.Builder request,
+        int maxReadRowsRetries,
+        Iterable<MockResponsesBatch> responses) {
+      super(clientFactory, request, maxReadRowsRetries);
+      this.responses = responses.iterator();
     }
 
-    @Test
-    public void testRetryOfSingleFailure()
-    {
-        MockResponsesBatch batch1 = new MockResponsesBatch();
-        batch1.addResponse(ReadRowsResponse.newBuilder().setRowCount(10).build());
-        batch1.addException(new StatusRuntimeException(Status.INTERNAL.withDescription(
-                "Received unexpected EOS on DATA frame from server.")));
-        MockResponsesBatch batch2 = new MockResponsesBatch();
-        batch2.addResponse(ReadRowsResponse.newBuilder().setRowCount(11).build());
-
-        ImmutableList<ReadRowsResponse> responses = ImmutableList.copyOf(
-                new MockReadRowsHelper(clientFactory, request, 3, ImmutableList.of(batch1, batch2))
-                        .readRows());
-
-        assertThat(responses.size()).isEqualTo(2);
-        assertThat(responses.stream().mapToLong(ReadRowsResponse::getRowCount).sum()).isEqualTo(21);
+    @Override
+    protected Iterator<ReadRowsResponse> fetchResponses(ReadRowsRequest.Builder readRowsRequest) {
+      return responses.next();
     }
-
-    private static final class MockReadRowsHelper
-            extends ReadRowsHelper
-    {
-        Iterator<MockResponsesBatch> responses;
-
-        MockReadRowsHelper(BigQueryReadClientFactory clientFactory, ReadRowsRequest.Builder request, int maxReadRowsRetries, Iterable<MockResponsesBatch> responses)
-        {
-            super(clientFactory, request, maxReadRowsRetries);
-            this.responses = responses.iterator();
-        }
-
-        @Override
-        protected Iterator<ReadRowsResponse> fetchResponses(ReadRowsRequest.Builder readRowsRequest)
-        {
-            return responses.next();
-        }
-    }
+  }
 }
--- connector/src/test/java/com/google/cloud/spark/bigquery/SparkBigQueryConfigTest.java
+++ connector/src/test/java/com/google/cloud/spark/bigquery/SparkBigQueryConfigTest.java
@@ -33,77 +33,86 @@ import static com.google.common.truth.Truth.assertThat;
 
 public class SparkBigQueryConfigTest {
 
-    ImmutableMap<String, String> defaultOptions = ImmutableMap.of(
-            "table", "dataset.table");
-            //"project", "test_project"); // to remove the need for default project
+  ImmutableMap<String, String> defaultOptions = ImmutableMap.of("table", "dataset.table");
+  // "project", "test_project"); // to remove the need for default project
 
-    @Test
-    public void testDefaults() {
-        Configuration hadoopConfiguration  = new Configuration();
-        DataSourceOptions options = new DataSourceOptions(defaultOptions);
-        SparkBigQueryConfig config = SparkBigQueryConfig.from(options, ImmutableMap.of(), hadoopConfiguration,10);
-        assertThat(config.getTableId()).isEqualTo(TableId.of("dataset", "table"));
-        assertThat(config.getFilter()).isEqualTo(Optional.empty());
-        assertThat(config.getSchema()).isEqualTo(Optional.empty());
-        assertThat(config.getMaxParallelism()).isEqualTo(OptionalInt.empty());
-        assertThat(config.getTemporaryGcsBucket()).isEqualTo(Optional.empty());
-        assertThat(config.getIntermediateFormat()).isEqualTo(SparkBigQueryConfig.DEFAULT_INTERMEDIATE_FORMAT);
-        assertThat(config.getReadDataFormat()).isEqualTo(SparkBigQueryConfig.DEFAULT_READ_DATA_FORMAT);
-        assertThat(config.getMaterializationProject()).isEqualTo(Optional.empty());
-        assertThat(config.getMaterializationDataset()).isEqualTo(Optional.empty());
-        assertThat(config.getPartitionField()).isEqualTo(Optional.empty());
-        assertThat(config.getPartitionExpirationMs()).isEqualTo(OptionalLong.empty());
-        assertThat(config.getPartitionRequireFilter()).isEqualTo(Optional.empty());
-        assertThat(config.getPartitionType()).isEqualTo(Optional.empty());
-        assertThat(config.getClusteredFields()).isEqualTo(Optional.empty());
-        assertThat(config.getCreateDisposition()).isEqualTo(Optional.empty());
-        assertThat(config.getLoadSchemaUpdateOptions()).isEqualTo(ImmutableList.of());
-        assertThat(config.getViewExpirationTimeInHours()).isEqualTo(24);
-        assertThat(config.getMaxReadRowsRetries()).isEqualTo(3);
-    }
+  @Test
+  public void testDefaults() {
+    Configuration hadoopConfiguration = new Configuration();
+    DataSourceOptions options = new DataSourceOptions(defaultOptions);
+    SparkBigQueryConfig config =
+        SparkBigQueryConfig.from(options, ImmutableMap.of(), hadoopConfiguration, 10);
+    assertThat(config.getTableId()).isEqualTo(TableId.of("dataset", "table"));
+    assertThat(config.getFilter()).isEqualTo(Optional.empty());
+    assertThat(config.getSchema()).isEqualTo(Optional.empty());
+    assertThat(config.getMaxParallelism()).isEqualTo(OptionalInt.empty());
+    assertThat(config.getTemporaryGcsBucket()).isEqualTo(Optional.empty());
+    assertThat(config.getIntermediateFormat())
+        .isEqualTo(SparkBigQueryConfig.DEFAULT_INTERMEDIATE_FORMAT);
+    assertThat(config.getReadDataFormat()).isEqualTo(SparkBigQueryConfig.DEFAULT_READ_DATA_FORMAT);
+    assertThat(config.getMaterializationProject()).isEqualTo(Optional.empty());
+    assertThat(config.getMaterializationDataset()).isEqualTo(Optional.empty());
+    assertThat(config.getPartitionField()).isEqualTo(Optional.empty());
+    assertThat(config.getPartitionExpirationMs()).isEqualTo(OptionalLong.empty());
+    assertThat(config.getPartitionRequireFilter()).isEqualTo(Optional.empty());
+    assertThat(config.getPartitionType()).isEqualTo(Optional.empty());
+    assertThat(config.getClusteredFields()).isEqualTo(Optional.empty());
+    assertThat(config.getCreateDisposition()).isEqualTo(Optional.empty());
+    assertThat(config.getLoadSchemaUpdateOptions()).isEqualTo(ImmutableList.of());
+    assertThat(config.getViewExpirationTimeInHours()).isEqualTo(24);
+    assertThat(config.getMaxReadRowsRetries()).isEqualTo(3);
+  }
 
-    @Test
-    public void testConfigFromOptions() {
-        Configuration hadoopConfiguration  = new Configuration();
-        DataSourceOptions options = new DataSourceOptions(ImmutableMap.<String, String>builder()
-                .put("table","test_t")
-                .put("dataset","test_d")
-                .put("project","test_p")
-                .put("filter","test > 0")
-                .put("parentProject","test_pp")
-                .put("maxParallelism","99")
-                .put("viewsEnabled","true")
-                .put("viewMaterializationProject","vmp")
-                .put("viewMaterializationDataset","vmd")
-                .put("readDataFormat","ARROW")
-                .put("optimizedEmptyProjection","false")
-                .put("createDisposition","CREATE_NEVER")
-                .put("temporaryGcsBucket","some_bucket")
-                .put("intermediateFormat","ORC")
+  @Test
+  public void testConfigFromOptions() {
+    Configuration hadoopConfiguration = new Configuration();
+    DataSourceOptions options =
+        new DataSourceOptions(
+            ImmutableMap.<String, String>builder()
+                .put("table", "test_t")
+                .put("dataset", "test_d")
+                .put("project", "test_p")
+                .put("filter", "test > 0")
+                .put("parentProject", "test_pp")
+                .put("maxParallelism", "99")
+                .put("viewsEnabled", "true")
+                .put("viewMaterializationProject", "vmp")
+                .put("viewMaterializationDataset", "vmd")
+                .put("readDataFormat", "ARROW")
+                .put("optimizedEmptyProjection", "false")
+                .put("createDisposition", "CREATE_NEVER")
+                .put("temporaryGcsBucket", "some_bucket")
+                .put("intermediateFormat", "ORC")
                 .put("partitionRequireFilter", "true")
-                .put("partitionField","some_field")
-                .put("partitionExpirationMs","999")
-                .put("clusteredFields","field1,field2")
-                .put("allowFieldAddition","true")
-                .put("allowFieldRelaxation","true")
-        .build());
-        SparkBigQueryConfig config = SparkBigQueryConfig.from(options, ImmutableMap.of(), hadoopConfiguration, 10);
-        assertThat(config.getTableId()).isEqualTo(TableId.of("test_p", "test_d", "test_t"));
-        assertThat(config.getFilter()).isEqualTo(Optional.of("test > 0"));
-        assertThat(config.getSchema()).isEqualTo(Optional.empty());
-        assertThat(config.getMaxParallelism()).isEqualTo(OptionalInt.of(99));
-        assertThat(config.getTemporaryGcsBucket()).isEqualTo(Optional.of("some_bucket"));
-        assertThat(config.getIntermediateFormat()).isEqualTo(FormatOptions.orc());
-        assertThat(config.getReadDataFormat()).isEqualTo(DataFormat.ARROW);
-        assertThat(config.getMaterializationProject()).isEqualTo(Optional.of("vmp"));
-        assertThat(config.getMaterializationDataset()).isEqualTo(Optional.of("vmd"));
-        assertThat(config.getPartitionField()).isEqualTo(Optional.of("some_field"));
-        assertThat(config.getPartitionExpirationMs()).isEqualTo(OptionalLong.of(999));
-        assertThat(config.getPartitionRequireFilter()).isEqualTo(Optional.of(true));
-        assertThat(config.getClusteredFields().get()).isEqualTo(new String[] {"field1", "field2"});
-        assertThat(config.getCreateDisposition()).isEqualTo(Optional.of(JobInfo.CreateDisposition.CREATE_NEVER));
-        assertThat(config.getLoadSchemaUpdateOptions()).isEqualTo(ImmutableList.of(JobInfo.SchemaUpdateOption.ALLOW_FIELD_ADDITION, JobInfo.SchemaUpdateOption.ALLOW_FIELD_RELAXATION));
-        assertThat(config.getViewExpirationTimeInHours()).isEqualTo(24);
-        assertThat(config.getMaxReadRowsRetries()).isEqualTo(3);
-    }
+                .put("partitionField", "some_field")
+                .put("partitionExpirationMs", "999")
+                .put("clusteredFields", "field1,field2")
+                .put("allowFieldAddition", "true")
+                .put("allowFieldRelaxation", "true")
+                .build());
+    SparkBigQueryConfig config =
+        SparkBigQueryConfig.from(options, ImmutableMap.of(), hadoopConfiguration, 10);
+    assertThat(config.getTableId()).isEqualTo(TableId.of("test_p", "test_d", "test_t"));
+    assertThat(config.getFilter()).isEqualTo(Optional.of("test > 0"));
+    assertThat(config.getSchema()).isEqualTo(Optional.empty());
+    assertThat(config.getMaxParallelism()).isEqualTo(OptionalInt.of(99));
+    assertThat(config.getTemporaryGcsBucket()).isEqualTo(Optional.of("some_bucket"));
+    assertThat(config.getIntermediateFormat()).isEqualTo(FormatOptions.orc());
+    assertThat(config.getReadDataFormat()).isEqualTo(DataFormat.ARROW);
+    assertThat(config.getMaterializationProject()).isEqualTo(Optional.of("vmp"));
+    assertThat(config.getMaterializationDataset()).isEqualTo(Optional.of("vmd"));
+    assertThat(config.getPartitionField()).isEqualTo(Optional.of("some_field"));
+    assertThat(config.getPartitionExpirationMs()).isEqualTo(OptionalLong.of(999));
+    assertThat(config.getPartitionRequireFilter()).isEqualTo(Optional.of(true));
+    assertThat(config.getClusteredFields().get()).isEqualTo(new String[] {"field1", "field2"});
+    assertThat(config.getCreateDisposition())
+        .isEqualTo(Optional.of(JobInfo.CreateDisposition.CREATE_NEVER));
+    assertThat(config.getLoadSchemaUpdateOptions())
+        .isEqualTo(
+            ImmutableList.of(
+                JobInfo.SchemaUpdateOption.ALLOW_FIELD_ADDITION,
+                JobInfo.SchemaUpdateOption.ALLOW_FIELD_RELAXATION));
+    assertThat(config.getViewExpirationTimeInHours()).isEqualTo(24);
+    assertThat(config.getMaxReadRowsRetries()).isEqualTo(3);
+  }
 }
