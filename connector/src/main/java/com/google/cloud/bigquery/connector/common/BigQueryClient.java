b44771ffb1829a261c0906f198efa88554b50f5c
Yuval Medina <ymed@google.com>
2020-07-23 18:25:21 +0000

    Added expiration time for temporary tables. Divided Save-Mode test into testing each individual save-mode. Added schema validation to DataSource Writer.

--- connector/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryClient.java
+++ connector/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryClient.java
@@ -90,7 +90,10 @@ public class BigQueryClient {
     String tempDataset = materializationDataset.orElseGet(destinationTableId::getDataset);
     TableId tempTableId =
         TableId.of(tempProject, tempDataset, destinationTableId.getTable() + System.nanoTime());
-    return createTable(tempTableId, schema);
+    // Build TableInfo with expiration time of one day from current epoch.
+    TableInfo tableInfo = TableInfo.newBuilder(tempTableId, StandardTableDefinition.of(schema))
+            .setExpirationTime(System.currentTimeMillis()+86400000L).build();
+    return bigQuery.create(tableInfo);
   }
 
   public Job overwriteDestinationWithTemporary(
