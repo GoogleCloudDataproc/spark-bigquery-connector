564722aed9d137f02ab037c42fe65546478bab60
Yuval Medina <ymed@google.com>
2020-08-04 17:28:49 +0000

    Added Google copyright notice.

--- connector/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryWriteClientModule.java
+++ connector/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryWriteClientModule.java
@@ -1,17 +1,26 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.bigquery.connector.common;
 
-import com.google.auth.Credentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.TableId;
 import com.google.cloud.spark.bigquery.SparkBigQueryConfig;
 import com.google.cloud.spark.bigquery.v2.BigQueryDataSourceWriter;
 import com.google.inject.*;
 import org.apache.spark.sql.SaveMode;
-import org.apache.spark.sql.sources.v2.DataSourceOptions;
 import org.apache.spark.sql.types.StructType;
-
-import java.util.Optional;
 
 public class BigQueryWriteClientModule implements Module {
 
--- connector/src/main/java/com/google/cloud/spark/bigquery/v2/BigQueryDataSourceWriter.java
+++ connector/src/main/java/com/google/cloud/spark/bigquery/v2/BigQueryDataSourceWriter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.spark.bigquery.v2;
 
 import com.google.cloud.bigquery.*;
@@ -5,10 +20,8 @@ import com.google.cloud.bigquery.connector.common.BigQueryClient;
 import com.google.cloud.bigquery.connector.common.BigQueryWriteClientFactory;
 import com.google.cloud.bigquery.storage.v1alpha2.BigQueryWriteClient;
 import com.google.cloud.bigquery.storage.v1alpha2.ProtoBufProto;
-import com.google.cloud.bigquery.storage.v1alpha2.ProtoSchemaConverter;
 import com.google.cloud.bigquery.storage.v1alpha2.Storage;
 import com.google.common.base.Preconditions;
-import com.google.protobuf.Descriptors;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.sources.v2.writer.DataSourceWriter;
@@ -20,7 +33,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
-import static com.google.cloud.spark.bigquery.ProtobufUtils.toDescriptor;
 import static com.google.cloud.spark.bigquery.ProtobufUtils.toProtoSchema;
 import static com.google.cloud.spark.bigquery.SchemaConverters.toBigQuerySchema;
 
