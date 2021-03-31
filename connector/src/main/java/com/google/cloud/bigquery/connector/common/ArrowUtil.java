615a22171fa3f75fa8d093864bde73ed97e0e0ee
Micah Kornfield <micahk@google.com>
2021-03-31 15:36:48 -0700

    add comments

--- connector/src/main/java/com/google/cloud/bigquery/connector/common/ArrowUtil.java
+++ connector/src/main/java/com/google/cloud/bigquery/connector/common/ArrowUtil.java
@@ -7,8 +7,12 @@ import org.apache.arrow.memory.NettyAllocationManager;
 public class ArrowUtil {
   private ArrowUtil() {};
 
+  /** Returns a new allocator limited to maxAllocation bytes */
   public static RootAllocator newRootAllocator(long maxAllocation) {
-
+    // Arrow has classpath scanning logic based on hard-coding strings to
+    // select the default allocationManagerFactory by selecting one
+    // here it should be properly shaded with the rest of Arrow within
+    // the connector.
     return new RootAllocator(
         RootAllocator.configBuilder()
             .allocationManagerFactory(NettyAllocationManager.FACTORY)
