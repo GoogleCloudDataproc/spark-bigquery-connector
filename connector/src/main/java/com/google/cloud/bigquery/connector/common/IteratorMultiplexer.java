fc86a21b67f905318110fe4e780b5c092b64a749
emkornfield <micahk@google.com>
2021-05-14 10:14:59 -0700

    Add iterator multiplexer (#378)

---
+++ connector/src/main/java/com/google/cloud/bigquery/connector/common/IteratorMultiplexer.java
@@ -0,0 +1,137 @@
+package com.google.cloud.bigquery.connector.common;
+
+import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Manages reading ahead from an iterator and dividing it across multiple iterators that can be read
+ * in a round-robin fashion.
+ *
+ * <p>Useful to parallelizing work from an iterator where order must still be maintained.
+ *
+ * @param <T> Type of iterable object.
+ */
+public class IteratorMultiplexer<T> implements AutoCloseable {
+  private static final Logger log = LoggerFactory.getLogger(IteratorMultiplexer.class);
+  private final Iterator<T> iterator;
+  private final int splits;
+  private final QueueIterator<T>[] iterators;
+  private Thread worker;
+  private volatile RuntimeException rethrow;
+
+  /**
+   * Construct a new instance.
+   *
+   * @param iterator The Iterator to read from.
+   * @param splits The number of output iterators that will read from iterator.
+   */
+  IteratorMultiplexer(Iterator<T> iterator, int splits) {
+    this.iterator = iterator;
+    this.splits = splits;
+    // Filled in when initializing iterators.
+    iterators = new QueueIterator[splits];
+    for (int x = 0; x < splits; x++) {
+      iterators[x] = new QueueIterator<>();
+    }
+  }
+
+  @Override
+  public void close() {
+    if (worker != null) {
+      worker.interrupt();
+      try {
+        worker.join(/*millis=*/ 1000);
+      } catch (InterruptedException e) {
+        throw new RuntimeException("Interrupted while waiting on worker thread shutdown.", e);
+      }
+      worker = null;
+      if (rethrow != null) {
+        throw new RuntimeException("Error while closing.", rethrow);
+      }
+    } else {
+      for (int x = 0; x < splits; x++) {
+        iterators[x].done.set(true);
+      }
+    }
+  }
+
+  void readAhead() {
+    try {
+      boolean hasMore = true;
+      while (hasMore) {
+        for (int x = 0; x < splits; x++) {
+          if (iterator.hasNext()) {
+            T value = iterator.next();
+            iterators[x].queue.put(value);
+          } else {
+            hasMore = false;
+            break;
+          }
+        }
+      }
+    } catch (InterruptedException e) {
+      log.info("Worker thread had error. Ending all iterators");
+      rethrow = new RuntimeException("Worker thread interrupted");
+    } catch (RuntimeException e) {
+      rethrow = e;
+    }
+    for (int x = 0; x < splits; x++) {
+      iterators[x].done.set(true);
+    }
+  }
+
+  public Iterator<T> getSplit(int split) {
+    if (worker == null) {
+      worker = new Thread(this::readAhead, "readahead-worker");
+      worker.setDaemon(true);
+      worker.start();
+    }
+    return iterators[split];
+  }
+
+  private class QueueIterator<T> implements Iterator<T> {
+    private final ArrayBlockingQueue<T> queue = new ArrayBlockingQueue<>(/*capacity=*/ 1);
+    private final AtomicBoolean done = new AtomicBoolean(false);
+
+    private T t = null;
+
+    @Override
+    public boolean hasNext() {
+      if (!mightHaveNext()) {
+        return false;
+      }
+      t = null;
+      try {
+        while (t == null && mightHaveNext()) {
+          t = queue.poll(10, TimeUnit.MILLISECONDS);
+        }
+      } catch (InterruptedException e) {
+        done.set(true);
+        // We expect all iterators to either make progress together or finish.
+        // This starts the cleanup process to halt all workers.
+        worker.interrupt();
+        throw new RuntimeException(e);
+      }
+      if (t == null) {
+        done.set(true);
+        return false;
+      }
+      return true;
+    }
+
+    private boolean mightHaveNext() {
+      return !done.get() || !queue.isEmpty();
+    }
+
+    @Override
+    public T next() {
+      Preconditions.checkState(t != null, "next element cannot be null");
+      return t;
+    }
+  }
+}
---
+++ connector/src/test/java/com/google/cloud/bigquery/connector/common/IteratorMultiplexerTest.java
@@ -0,0 +1,129 @@
+package com.google.cloud.bigquery.connector.common;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+public class IteratorMultiplexerTest {
+
+  @Test
+  public void testIteratorRoundRobins() throws InterruptedException {
+    ImmutableList<Integer> values = ImmutableList.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    try (IteratorMultiplexer multiplexer =
+        new IteratorMultiplexer<>(values.iterator(), /*multiplexedIterators=*/ 3)) {
+      ImmutableList<Iterator<Integer>> iterators =
+          ImmutableList.of(
+              multiplexer.getSplit(0), multiplexer.getSplit(1), multiplexer.getSplit(2));
+      ImmutableList<List<Integer>> multiPlexed =
+          ImmutableList.of(new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+      ExecutorService executorService = Executors.newFixedThreadPool(3);
+      for (int x = 0; x < 3; x++) {
+        final int idx = x;
+        executorService.submit(
+            () -> {
+              Iterator<Integer> iterator = iterators.get(idx);
+              while (iterator.hasNext()) {
+                multiPlexed.get(idx).add(iterator.next());
+              }
+            });
+      }
+      executorService.shutdown();
+      assertThat(executorService.awaitTermination(200, TimeUnit.MILLISECONDS)).isTrue();
+      assertThat(multiPlexed.get(0)).containsExactly(0, 3, 6, 9).inOrder();
+      assertThat(multiPlexed.get(1)).containsExactly(1, 4, 7).inOrder();
+      assertThat(multiPlexed.get(2)).containsExactly(2, 5, 8).inOrder();
+    }
+  }
+
+  @Test
+  public void testIteratorClosedGracefullyWhenSubIteratorsAreInterrupted()
+      throws InterruptedException {
+    Iterator<Integer> infiniteIterator =
+        new Iterator<Integer>() {
+          @Override
+          public boolean hasNext() {
+            return true;
+          }
+
+          @Override
+          public Integer next() {
+            return 0;
+          }
+        };
+    try (IteratorMultiplexer multiplexer =
+        new IteratorMultiplexer(infiniteIterator, /*multiplexedIterators=*/ 3)) {
+      ImmutableList<Iterator<Integer>> iterators =
+          ImmutableList.of(
+              multiplexer.getSplit(0), multiplexer.getSplit(1), multiplexer.getSplit(2));
+      ExecutorService executorService = Executors.newFixedThreadPool(3);
+      for (int x = 0; x < 3; x++) {
+        final int idx = x;
+        executorService.submit(
+            () -> {
+              Iterator<Integer> iterator = iterators.get(idx);
+              while (iterator.hasNext()) {
+                iterator.next();
+              }
+            });
+      }
+      executorService.shutdownNow();
+      assertThat(executorService.awaitTermination(100, TimeUnit.MILLISECONDS)).isTrue();
+      assertThat(executorService.isTerminated());
+      assertThrows(RuntimeException.class, multiplexer::close);
+    }
+  }
+
+  @Test
+  public void testIteratorClosedGracefullyWhenMultiplexerClosed() throws InterruptedException {
+    Iterator<Integer> infiniteIterator =
+        new Iterator<Integer>() {
+          @Override
+          public boolean hasNext() {
+            try {
+              TimeUnit.MINUTES.sleep(1);
+              return true;
+            } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+          }
+
+          @Override
+          public Integer next() {
+            return 0;
+          }
+        };
+    CountDownLatch exited = new CountDownLatch(3);
+    try (IteratorMultiplexer multiplexer =
+        new IteratorMultiplexer(infiniteIterator, /*multiplexedIterators=*/ 3)) {
+      ImmutableList<Iterator<Integer>> iterators =
+          ImmutableList.of(
+              multiplexer.getSplit(0), multiplexer.getSplit(1), multiplexer.getSplit(2));
+      ExecutorService executorService = Executors.newFixedThreadPool(3);
+      for (int x = 0; x < 3; x++) {
+        final int idx = x;
+        executorService.submit(
+            () -> {
+              Iterator<Integer> iterator = iterators.get(idx);
+              while (iterator.hasNext()) {
+                iterator.next();
+              }
+              exited.countDown();
+            });
+      }
+      Exception thrown = assertThrows(Exception.class, multiplexer::close);
+
+      assertThat(exited.await(3, TimeUnit.SECONDS)).isTrue();
+      executorService.shutdown();
+    }
+  }
+}
