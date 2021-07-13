927fe7d8931e3c4074936a9887a311c593ad518f
David Rabinowitz <davidrab@google.com>
2021-07-12 20:36:29 -0700

    fixing package

--- spark-bigquery-connector-common/src/test/java/com/google/cloud/spark/bigquery/acceptance/AcceptanceTestContext.java
+++ spark-bigquery-connector-common/src/test/java/com/google/cloud/spark/bigquery/acceptance/AcceptanceTestContext.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.spark.bigquery.acceptance;
+package com.google.cloud.spark.bigquery.acceptance;
 
 public class AcceptanceTestContext {
 
