1dae6304eea7852b6ba50ada62d9fe1f5587d2e8
BhagyaaShreeS <bsathyamurthy@whirldatascience.com>
2021-10-11 16:49:43 +0530

    Adding common project dependency for spark24

--- spark-bigquery-dsv2/spark-bigquery-spark24/src/main/java/com/google/cloud/spark/bigquery/v2/BigQueryInputPartitionReader.java
+++ spark-bigquery-dsv2/spark-bigquery-spark24/src/main/java/com/google/cloud/spark/bigquery/v2/BigQueryInputPartitionReader.java
@@ -18,6 +18,7 @@ package com.google.cloud.spark.bigquery.v2;
 import com.google.cloud.bigquery.connector.common.ReadRowsHelper;
 import com.google.cloud.bigquery.storage.v1.ReadRowsResponse;
 import com.google.cloud.spark.bigquery.ReadRowsResponseToInternalRowIteratorConverter;
+import com.google.cloud.spark.bigquery.common.GenericBigQueryInputPartitionReader;
 import com.google.common.collect.ImmutableList;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.sources.v2.reader.InputPartitionReader;
