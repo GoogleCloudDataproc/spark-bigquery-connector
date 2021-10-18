447f95b38af88100ab2e77bfda61ea2f8582a3da
Praful Jain <jain.praful6465@gmail.com>
2021-10-18 17:32:32 +0530

    added generic spark session helper class in spark bigquery connector common

--- spark-bigquery-connector-common/src/main/java/com/google/cloud/spark/bigquery/SparkSessionHelper.java
+++ spark-bigquery-connector-common/src/main/java/com/google/cloud/spark/bigquery/SparkSessionHelper.java
@@ -1,15 +1,34 @@
 package com.google.cloud.spark.bigquery;
 
+import com.google.cloud.bigquery.connector.common.BigQueryClientModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.sources.v2.DataSourceOptions;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.Map;
+import java.util.Optional;
 
 
 public class SparkSessionHelper {
+    // This method is used to create spark session
     public static SparkSession getDefaultSparkSessionOrCreate() {
         scala.Option<SparkSession> defaultSpareSession = SparkSession.getActiveSession();
         if (defaultSpareSession.isDefined()) {
             return defaultSpareSession.get();
         }
         return SparkSession.builder().appName("spark-bigquery-connector").getOrCreate();
+    }
+    // This method is used to create injection by providing
+    public static Injector createInjector(StructType schema, Map<String, String> options, Module module) {
+        SparkSession spark = SparkSessionHelper.getDefaultSparkSessionOrCreate();
+        return Guice.createInjector(
+                new BigQueryClientModule(),
+                new SparkBigQueryConnectorModule(
+                        spark, options, Optional.ofNullable(schema), DataSourceVersion.V2),
+                module);
     }
 
 }
