d776d3566f2179fb50143df56635b84a3bf59f46
suryasoma <suryasoma.9@gmail.com>
2022-05-20 12:33:28 -0700

    addressing review comments

--- spark-bigquery-tests/src/main/java/com/google/cloud/spark/bigquery/integration/QueryPushdownIntegrationTestBase.java
+++ spark-bigquery-tests/src/main/java/com/google/cloud/spark/bigquery/integration/QueryPushdownIntegrationTestBase.java
@@ -14,32 +14,31 @@ public class QueryPushdownIntegrationTestBase extends SparkBigQueryIntegrationTe
     Dataset<Row> df = spark.read().format("bigquery").load(TestConstants.SHAKESPEARE_TABLE);
     df =
         df.selectExpr(
-            "word",
-            "ASCII(word) as ascii",
-            "LENGTH(word) as length",
-            "LOWER(word) as lower",
-            "LPAD(word, 10, '*') as lpad",
-            "RPAD(word, 10, '*') as rpad",
-            "TRANSLATE(word, 'a', '*') as translate",
-            "TRIM(concat('    ', word, '    ')) as trim",
-            "LTRIM(concat('    ', word, '    ')) as ltrim",
-            "RTRIM(concat('    ', word, '    ')) as rtrim",
-            "UPPER(word) as upper",
-            "INSTR(word, 'a') as instr",
-            "INITCAP(word) as initcap",
-            "CONCAT(word, '*', '!!') as concat",
-            "BASE64(word) as base",
-            "UNBASE64(word) as base",
-            "FORMAT_STRING('*%s*', word) as format_string",
-            "FORMAT_NUMBER(10.2345, 1) as format_number",
-            "REGEXP_EXTRACT(word, '([A-Za-z]+$)', 1) as regexp_extract",
-            "REGEXP_REPLACE(word, '([A-Za-z]+$)', 'replacement') as regexp_replace",
-            "SUBSTR(word, 2, 2) as substr",
-            "SOUNDEX(word) as soundex");
-    df = df.limit(2);
+                "word",
+                "ASCII(word) as ascii",
+                "LENGTH(word) as length",
+                "LOWER(word) as lower",
+                "LPAD(word, 10, '*') as lpad",
+                "RPAD(word, 10, '*') as rpad",
+                "TRANSLATE(word, 'a', '*') as translate",
+                "TRIM(concat('    ', word, '    ')) as trim",
+                "LTRIM(concat('    ', word, '    ')) as ltrim",
+                "RTRIM(concat('    ', word, '    ')) as rtrim",
+                "UPPER(word) as upper",
+                "INSTR(word, 'a') as instr",
+                "INITCAP(word) as initcap",
+                "CONCAT(word, '*', '!!') as concat",
+                "BASE64(word) as base",
+                "UNBASE64(word) as base",
+                "FORMAT_STRING('*%s*', word) as format_string",
+                "FORMAT_NUMBER(10.2345, 1) as format_number",
+                "REGEXP_EXTRACT(word, '([A-Za-z]+$)', 1) as regexp_extract",
+                "REGEXP_REPLACE(word, '([A-Za-z]+$)', 'replacement') as regexp_replace",
+                "SUBSTR(word, 2, 2) as substr",
+                "SOUNDEX(word) as soundex")
+            .where("word = 'augurs'");
     List<Row> result = df.collectAsList();
-    assertThat(result.size()).isEqualTo(2);
-    Row r1 = result.get(1);
+    Row r1 = result.get(0);
     assertThat(r1.get(0)).isEqualTo("augurs"); // word
     assertThat(r1.get(1)).isEqualTo(97); // ASCII(word)
     assertThat(r1.get(2)).isEqualTo(6); // LENGTH(word)
