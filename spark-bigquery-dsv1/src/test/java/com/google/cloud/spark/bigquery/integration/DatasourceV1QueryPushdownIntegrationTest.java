155721c8f2afc262df8f4737542203e19575bf7a
suryasoma <suryasoma.9@gmail.com>
2022-05-19 18:02:40 -0700

    spotless applied

--- spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DatasourceV1QueryPushdownIntegrationTest.java
+++ spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DatasourceV1QueryPushdownIntegrationTest.java
@@ -2,7 +2,7 @@ package com.google.cloud.spark.bigquery.integration;
 
 import com.google.cloud.spark.bigquery.BigQueryConnectorUtils;
 
-public class DatasourceV1QueryPushdownIntegrationTest extends QueryPushdownIntegrationTestBase{
+public class DatasourceV1QueryPushdownIntegrationTest extends QueryPushdownIntegrationTestBase {
 
   public DatasourceV1QueryPushdownIntegrationTest() {
     BigQueryConnectorUtils.enablePushdownSession(spark);
