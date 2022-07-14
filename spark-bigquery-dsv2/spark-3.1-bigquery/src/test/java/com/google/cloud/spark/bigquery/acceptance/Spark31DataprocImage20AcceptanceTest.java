686185c320aea5731955a951137c9909be58728d
David Rabinowitz <davidrab@google.com>
2022-07-13 22:59:33 -0700

    Spark 3.1 acceptance tests

---
+++ spark-bigquery-dsv2/spark-3.1-bigquery/src/test/java/com/google/cloud/spark/bigquery/acceptance/Spark31DataprocImage20AcceptanceTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.spark.bigquery.acceptance;
+
+import java.util.Collections;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class Spark31DataprocImage20AcceptanceTest extends DataprocAcceptanceTestBase {
+
+  private static AcceptanceTestContext context;
+
+  public Spark31DataprocImage20AcceptanceTest() {
+    super(context, false);
+  }
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    context =
+        DataprocAcceptanceTestBase.setup(
+            "2.0-debian10", "spark-3.1-bigquery", Collections.emptyList());
+  }
+
+  @AfterClass
+  public static void tearDown() throws Exception {
+    DataprocAcceptanceTestBase.tearDown(context);
+  }
+}
