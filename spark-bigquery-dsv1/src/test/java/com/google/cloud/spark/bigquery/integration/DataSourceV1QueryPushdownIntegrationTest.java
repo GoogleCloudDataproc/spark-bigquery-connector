f756f3988a6ca29b35dc0dbb2e0e9994765dcfcd
Vinay Londhe <vinaylondhe@google.com>
2022-07-19 20:19:02 +0530

    Remove integration test added inadvertently

--- spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
+++ spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
@@ -1,16 +1,10 @@
 package com.google.cloud.spark.bigquery.integration;
 
 import com.google.cloud.spark.bigquery.pushdowns.BigQueryConnectorUtils;
-import org.junit.Test;
 
 public class DataSourceV1QueryPushdownIntegrationTest extends QueryPushdownIntegrationTestBase {
 
   public DataSourceV1QueryPushdownIntegrationTest() {
     BigQueryConnectorUtils.enablePushdownSession(spark);
-  }
-
-  @Test
-  public void testSomething() {
-    testMiscellaneousExpressions();
   }
 }
