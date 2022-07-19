e94a25b681c5212bc3c6db199043b0625be98ca3
Vinay Londhe <vinaylondhe@google.com>
2022-07-19 18:36:13 +0530

    Added integration tests for CheckOverflow and UnaryMinus

--- spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
+++ spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
@@ -1,10 +1,16 @@
 package com.google.cloud.spark.bigquery.integration;
 
 import com.google.cloud.spark.bigquery.pushdowns.BigQueryConnectorUtils;
+import org.junit.Test;
 
 public class DataSourceV1QueryPushdownIntegrationTest extends QueryPushdownIntegrationTestBase {
 
   public DataSourceV1QueryPushdownIntegrationTest() {
     BigQueryConnectorUtils.enablePushdownSession(spark);
+  }
+
+  @Test
+  public void testSomething() {
+    testMiscellaneousExpressions();
   }
 }
