4434d1620bc40572a65267cea2006b89d746de2d
Vinay Londhe <vinaylondhe@google.com>
2022-08-01 17:02:51 -0700

    Change the path of BigQueryConnectorUtils in the python library

--- spark-bigquery-python-lib/src/main/python/google/cloud/spark/bigquery/big_query_connector_utils.py
+++ spark-bigquery-python-lib/src/main/python/google/cloud/spark/bigquery/big_query_connector_utils.py
@@ -1,6 +1,6 @@
 def enablePushdownSession(spark):
-  spark.sparkContext._jvm.com.google.cloud.spark.bigquery.BigQueryConnectorUtils.enablePushdownSession(spark._jsparkSession)
+  spark.sparkContext._jvm.com.google.cloud.spark.bigquery.pushdowns.BigQueryConnectorUtils.enablePushdownSession(spark._jsparkSession)
 
 
 def disablePushdownSession(spark):
-  spark.sparkContext._jvm.com.google.cloud.spark.bigquery.BigQueryConnectorUtils.disablePushdownSession(spark._jsparkSession)
+  spark.sparkContext._jvm.com.google.cloud.spark.bigquery.pushdowns.BigQueryConnectorUtils.disablePushdownSession(spark._jsparkSession)
