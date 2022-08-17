62041eeec5e0c3d1b9f73fa5be5db77c56f75479
David Rabinowitz <davidrab@google.com>
2022-08-17 13:39:13 -0700

    Fixing Spark3SqlUtils access issue

--- spark-bigquery-dsv1/spark-bigquery-dsv1-spark3-support/src/main/java/org/apache/spark/sql/Spark3SqlUtils.java
+++ spark-bigquery-dsv1/spark-bigquery-dsv1-spark3-support/src/main/java/org/apache/spark/sql/Spark3SqlUtils.java
@@ -16,16 +16,35 @@
 package org.apache.spark.sql;
 
 import org.apache.spark.sql.catalyst.expressions.AttributeReference;
+import org.apache.spark.sql.catalyst.expressions.NamedExpression;
 import org.apache.spark.sql.types.StructType;
+import scala.collection.JavaConverters;
 import scala.collection.Seq;
+import scala.collection.Seq$;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Spark3SqlUtils {
 
   private Spark3SqlUtils() {}
 
   // `toAttributes` is protected[sql] starting spark 3.2.0, so we need this call to be in the same
-  // package
+  // package. Since Scala 2.13/Spark 3.3 forbids it, the implementation has been ported to Java
   public static Seq<AttributeReference> toAttributes(StructType schema) {
-    return schema.toAttributes();
+    List<AttributeReference> result =
+        Stream.of(schema.fields())
+            .map(
+                field ->
+                    new AttributeReference(
+                        field.name(),
+                        field.dataType(),
+                        field.nullable(),
+                        field.metadata(),
+                        NamedExpression.newExprId(),
+                        Seq$.MODULE$.<String>newBuilder().result()))
+            .collect(Collectors.toList());
+    return JavaConverters.asScalaBuffer(result).toSeq();
   }
 }
