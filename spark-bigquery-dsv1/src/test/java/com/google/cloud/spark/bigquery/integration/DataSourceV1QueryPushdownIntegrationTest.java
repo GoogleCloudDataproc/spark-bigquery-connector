ac5fffd1a58067a2381f5206957dab3698c82e6c
suryasoma <suryasoma.9@gmail.com>
2022-08-24 16:07:22 -0700

    addressing review comments

--- spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
+++ spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.google.cloud.spark.bigquery.integration;
 
 import com.google.cloud.spark.bigquery.BigQueryConnectorUtils;
+import org.junit.Before;
 
 public class DataSourceV1QueryPushdownIntegrationTest extends QueryPushdownIntegrationTestBase {
 
-  public DataSourceV1QueryPushdownIntegrationTest() {
+  @Before
+  public void before() {
     BigQueryConnectorUtils.enablePushdownSession(spark);
   }
 }
--- spark-bigquery-dsv2/spark-3.1-bigquery/src/test/java/com/google/cloud/spark/bigquery/integration/Spark31QueryPushdownIntegrationTest.java
+++ spark-bigquery-dsv2/spark-3.1-bigquery/src/test/java/com/google/cloud/spark/bigquery/integration/Spark31QueryPushdownIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.google.cloud.spark.bigquery.integration;
 
 import com.google.cloud.spark.bigquery.BigQueryConnectorUtils;
+import org.junit.Before;
 
 public class Spark31QueryPushdownIntegrationTest extends QueryPushdownIntegrationTestBase {
-  public Spark31QueryPushdownIntegrationTest() {
+
+  @Before
+  public void before() {
     BigQueryConnectorUtils.enablePushdownSession(spark);
   }
 }
