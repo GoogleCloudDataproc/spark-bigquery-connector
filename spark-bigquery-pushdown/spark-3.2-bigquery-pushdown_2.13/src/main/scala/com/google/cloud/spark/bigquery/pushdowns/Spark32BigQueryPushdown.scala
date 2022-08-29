bd4013e9f3829e2b9348de79b6e88436d0d445cb
David Rabinowitz <davidrab@google.com>
2022-08-29 16:17:36 -0700

    Adding Scala 2.13 support part 1 - pushdown

---
+++ spark-bigquery-pushdown/spark-3.2-bigquery-pushdown_2.13/src/main/scala/com/google/cloud/spark/bigquery/pushdowns/Spark32BigQueryPushdown.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spark.bigquery.pushdowns
+
+class Spark32BigQueryPushdown extends BaseSparkBigQueryPushdown {
+
+  override def supportsSparkVersion(sparkVersion: String): Boolean = {
+    sparkVersion.startsWith("3.2")
+  }
+
+  override def createSparkExpressionConverter(expressionFactory: SparkExpressionFactory, sparkPlanFactory: SparkPlanFactory): SparkExpressionConverter = {
+    new Spark32ExpressionConverter(expressionFactory, sparkPlanFactory)
+  }
+
+  override def createSparkExpressionFactory: SparkExpressionFactory = {
+    new Spark32ExpressionFactory
+  }
+
+  override def createBigQueryStrategy(expressionConverter: SparkExpressionConverter, expressionFactory: SparkExpressionFactory, sparkPlanFactory: SparkPlanFactory): BigQueryStrategy = {
+    new Spark32BigQueryStrategy(expressionConverter, expressionFactory, sparkPlanFactory)
+  }
+
+  override def createSparkPlanFactory(): SparkPlanFactory = {
+    new Spark32PlanFactory
+  }
+}
