037882e35a4a9121b9ad23df4fb36bad92abea60
David Rabinowitz <davidrab@google.com>
2022-09-12 17:25:31 -0700

    fixed license headers

--- spark-bigquery-dsv1/spark-bigquery-with-dependencies_2.13/src/test/java/com/google/cloud/spark/bigquery/acceptance/Scala213DataprocImage21AcceptanceTest.java
+++ spark-bigquery-dsv1/spark-bigquery-with-dependencies_2.13/src/test/java/com/google/cloud/spark/bigquery/acceptance/Scala213DataprocImage21AcceptanceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google Inc. All Rights Reserved.
+ * Copyright 2022 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-package com.google.cloud.spark.bigquery.acceptance;
+ */package com.google.cloud.spark.bigquery.acceptance;
 
 import java.util.Collections;
 import org.junit.AfterClass;
