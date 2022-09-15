d04aa7777dd9e6ffc3d8be44836c7f1e3ac32c97
Vinay Londhe <vinaylondhe@google.com>
2022-09-14 20:06:43 -0700

    Increase number of partitions to 138 for natality table

--- spark-bigquery-tests/src/main/java/com/google/cloud/spark/bigquery/integration/ReadByFormatIntegrationTestBase.java
+++ spark-bigquery-tests/src/main/java/com/google/cloud/spark/bigquery/integration/ReadByFormatIntegrationTestBase.java
@@ -33,7 +33,7 @@ import org.junit.Test;
 
 public class ReadByFormatIntegrationTestBase extends SparkBigQueryIntegrationTestBase {
 
-  private static final int LARGE_TABLE_NUMBER_OF_PARTITIONS = 69;
+  private static final int LARGE_TABLE_NUMBER_OF_PARTITIONS = 138;
   protected final String dataFormat;
   protected final boolean userProvidedSchemaAllowed;
 
