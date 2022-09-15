2a7b6948af9213b23462a01d8d85421dba22e1fa
David Rabinowitz <davidrabinowitz@users.noreply.github.com>
2022-09-15 13:18:31 -0700

    Fixed Scala 2.11 JavaConverters issue (#782)

--- spark-bigquery-dsv1/spark-bigquery-dsv1-spark2-support/src/main/java/com/google/cloud/spark/bigquery/spark2/Spark2DataFrameToRDDConverter.java
+++ spark-bigquery-dsv1/spark-bigquery-dsv1-spark2-support/src/main/java/com/google/cloud/spark/bigquery/spark2/Spark2DataFrameToRDDConverter.java
@@ -17,8 +17,7 @@ package com.google.cloud.spark.bigquery.spark2;
 
 import com.google.cloud.spark.bigquery.DataFrameToRDDConverter;
 import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -27,24 +26,28 @@ import org.apache.spark.sql.catalyst.analysis.SimpleAnalyzer$;
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder;
 import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 import org.apache.spark.sql.catalyst.expressions.Attribute;
+import org.apache.spark.sql.catalyst.expressions.AttributeReference;
 import org.apache.spark.sql.types.StructType;
-import scala.collection.JavaConverters;
+import scala.collection.Seq;
+import scala.collection.mutable.WrappedArray;
 
 public class Spark2DataFrameToRDDConverter implements DataFrameToRDDConverter {
 
   @Override
   public RDD<Row> convertToRDD(Dataset<Row> data) {
     StructType schema = data.schema();
-    List<Attribute> attributes =
-        JavaConverters.asJavaCollection(schema.toAttributes()).stream()
-            .map(Attribute::toAttribute)
-            .collect(Collectors.toList());
+    // Going to more explicit API as JavaConverters changed significantly across Scala versions
+    Seq<AttributeReference> attributeReferenceSeq = schema.toAttributes();
+    AttributeReference[] attributeReferenceArray =
+        new AttributeReference[attributeReferenceSeq.size()];
+    attributeReferenceSeq.copyToArray(attributeReferenceArray);
+    Attribute[] attributes =
+        Stream.of(attributeReferenceArray).map(Attribute::toAttribute).toArray(Attribute[]::new);
+
+    Seq<Attribute> attributeSeq = WrappedArray.<Attribute>make(attributes).toSeq();
 
     final ExpressionEncoder<Row> expressionEncoder =
-        RowEncoder.apply(schema)
-            .resolveAndBind(
-                JavaConverters.asScalaIteratorConverter(attributes.iterator()).asScala().toSeq(),
-                SimpleAnalyzer$.MODULE$);
+        RowEncoder.apply(schema).resolveAndBind(attributeSeq, SimpleAnalyzer$.MODULE$);
 
     RDD<Row> rowRdd =
         data.queryExecution()
