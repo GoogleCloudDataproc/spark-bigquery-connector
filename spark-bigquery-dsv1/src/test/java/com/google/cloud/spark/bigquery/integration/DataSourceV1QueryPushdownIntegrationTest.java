14935d93fa10e8ee825d794f48c087bd231f55ba
Vinay Londhe <vinaylondhe@google.com>
2022-10-13 10:11:36 -0700

    Remove extraneous test added mistakenly

--- spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
+++ spark-bigquery-dsv1/src/test/java/com/google/cloud/spark/bigquery/integration/DataSourceV1QueryPushdownIntegrationTest.java
@@ -17,17 +17,11 @@ package com.google.cloud.spark.bigquery.integration;
 
 import com.google.cloud.spark.bigquery.BigQueryConnectorUtils;
 import org.junit.Before;
-import org.junit.Test;
 
 public class DataSourceV1QueryPushdownIntegrationTest extends QueryPushdownIntegrationTestBase {
 
   @Before
   public void before() {
     BigQueryConnectorUtils.enablePushdownSession(spark);
-  }
-
-  @Test
-  public void testSomething() {
-    testStringFunctionExpressions();
   }
 }
