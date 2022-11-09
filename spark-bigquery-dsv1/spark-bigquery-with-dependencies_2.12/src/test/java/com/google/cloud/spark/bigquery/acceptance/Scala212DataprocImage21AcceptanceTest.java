0ad221b9db358fac6477472cc513a69d8dc9ac63
David Rabinowitz <davidrab@google.com>
2022-11-08 22:03:58 -0800

    Adding Spark 3.3/Image 2.1 acceptance tests

---
+++ spark-bigquery-dsv1/spark-bigquery-with-dependencies_2.12/src/test/java/com/google/cloud/spark/bigquery/acceptance/Scala212DataprocImage21AcceptanceTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.spark.bigquery.acceptance;
+
+import java.util.Collections;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class Scala212DataprocImage21AcceptanceTest extends DataprocAcceptanceTestBase {
+
+  private static AcceptanceTestContext context;
+
+  public Scala212DataprocImage21AcceptanceTest() {
+    super(context);
+  }
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    context =
+        DataprocAcceptanceTestBase.setup(
+            "preview-debian11", "spark-bigquery", Collections.emptyList());
+  }
+
+  @AfterClass
+  public static void tearDown() throws Exception {
+    DataprocAcceptanceTestBase.tearDown(context);
+  }
+}
