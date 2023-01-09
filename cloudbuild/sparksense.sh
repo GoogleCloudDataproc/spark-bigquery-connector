15dcc188c8eeb19650b5f7c7dc35bd7616ac4962
suryasoma <suryasoma.9@gmail.com>
2023-01-09 14:52:08 -0800

    Added test runs for dsv2, gcs connector as well

--- cloudbuild/sparksense.sh
+++ cloudbuild/sparksense.sh
@@ -6,10 +6,30 @@ echo "start"
 gcloud config set project google.com:hadoop-cloud-dev
 gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
           --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
-          --cluster=sparksense-v1-bq-3 --region=us-central1 \
+          --cluster=sparksense-v1-20-bq --region=us-central1 \
           -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
           --bqJarLocation=gs://$bucket/spark-bigquery-with-dependencies_2.12-nightly-snapshot-sparksense.jar \
           --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
-          --bqTablePath=surya_spark_sense_test  --tempBucket=suryasoma-public \
+          --bqTablePath=spark_sense_tpcds_bigquery_runID  --tempBucket=suryasoma-public \
           --runType=bq --database=tpcds_1T_partitioned_gcs --runID=V1_20_BQ --queriesToRun=q1,q2
+
+
+gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
+          --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
+          --cluster=sparksense-v2-20-bq --region=us-central1 \
+          -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
+          --bqJarLocation=gs://$bucket/spark-2.4-bigquery-nightly-snapshot-preview-sparksense.jar \
+          --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
+          --bqTablePath=spark_sense_tpcds_bigquery_runID  --tempBucket=suryasoma-public \
+          --runType=bq --database=tpcds_1T_partitioned_gcs --runID=V2_20_BQ --queriesToRun=q1,q2
+
+
+gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
+          --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
+          --cluster=sparksense-20-gcs --region=us-central1 \
+          -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
+          --bqJarLocation=gs://$bucket/spark-bigquery-with-dependencies_2.12-nightly-snapshot-sparksense.jar \
+          --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
+          --bqTablePath=spark_sense_tpcds_bigquery_runID  --tempBucket=suryasoma-public \
+          --runType=bq --database=tpcds_1T_partitioned_gcs --runID=GCS_20 --queriesToRun=q1,q2
 echo "done"
