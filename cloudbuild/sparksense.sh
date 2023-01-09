3590fb9dc84a7fa68fbf78c12d6dcd1fec4d9904
suryasoma <suryasoma.9@gmail.com>
2023-01-09 15:03:46 -0800

    running spark sense parallely and changing cloud bucket

--- cloudbuild/sparksense.sh
+++ cloudbuild/sparksense.sh
@@ -1,20 +1,24 @@
 set -euxo pipefail
 
 readonly bucket=$1
+readonly STEP=$2
 
 echo "start"
 gcloud config set project google.com:hadoop-cloud-dev
-gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
-          --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
-          --cluster=sparksense-v1-20-bq --region=us-central1 \
-          -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
-          --bqJarLocation=gs://$bucket/spark-bigquery-with-dependencies_2.12-nightly-snapshot-sparksense.jar \
-          --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
-          --bqTablePath=spark_sense_tpcds_bigquery_runID  --tempBucket=suryasoma-public \
-          --runType=bq --database=tpcds_1T_partitioned_gcs --runID=V1_20_BQ --queriesToRun=q1,q2
+case $STEP in
+  v1-20-bq)
+    gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
+              --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
+              --cluster=sparksense-v1-20-bq --region=us-central1 \
+              -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
+              --bqJarLocation=gs://$bucket/spark-bigquery-with-dependencies_2.12-nightly-snapshot-sparksense.jar \
+              --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
+              --bqTablePath=spark_sense_tpcds_bigquery_runID  --tempBucket=suryasoma-public \
+              --runType=bq --database=tpcds_1T_partitioned_gcs --runID=V1_20_BQ --queriesToRun=q1,q2
+              ;;
 
-
-gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
+  v2-20-bq)
+    gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
           --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
           --cluster=sparksense-v2-20-bq --region=us-central1 \
           -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
@@ -22,9 +26,10 @@ gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=
           --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
           --bqTablePath=spark_sense_tpcds_bigquery_runID  --tempBucket=suryasoma-public \
           --runType=bq --database=tpcds_1T_partitioned_gcs --runID=V2_20_BQ --queriesToRun=q1,q2
+          ;;
 
-
-gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
+  gcs-20)
+    gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
           --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
           --cluster=sparksense-20-gcs --region=us-central1 \
           -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
@@ -32,4 +37,5 @@ gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=
           --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
           --bqTablePath=spark_sense_tpcds_bigquery_runID  --tempBucket=suryasoma-public \
           --runType=bq --database=tpcds_1T_partitioned_gcs --runID=GCS_20 --queriesToRun=q1,q2
+          ;;
 echo "done"
