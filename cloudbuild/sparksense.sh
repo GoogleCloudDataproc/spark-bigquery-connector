0886c1b5da1b8af68ed247d94a34b20db3d763d0
suryasoma <suryasoma.9@gmail.com>
2023-01-09 14:30:07 -0800

    changing cluster

--- cloudbuild/sparksense.sh
+++ cloudbuild/sparksense.sh
@@ -6,7 +6,7 @@ echo "start"
 gcloud config set project google.com:hadoop-cloud-dev
 gcloud dataproc jobs submit spark --project=google.com:hadoop-cloud-dev --class=com.google.cloud.performance.BenchmarkRunner \
           --jars=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
-          --cluster=sparksense-v1-20-bq --region=us-central1 \
+          --cluster=sparksense-v1-bq-3 --region=us-central1 \
           -- --benchmark=tpcds --jarLocation=gs://suryasoma-public/sparksense/jars/runID/spark-benchmark-kit-1.0-SNAPSHOT-jar-with-dependencies.jar \
           --bqJarLocation=gs://$bucket/spark-bigquery-with-dependencies_2.12-nightly-snapshot-sparksense.jar \
           --resultLocation=gs://suryasoma-public/sparksense/tpcds/bq --iterations=3 \
