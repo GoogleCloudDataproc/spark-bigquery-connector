9e31d999f8c7e80121b48a32a24d0d28c64189c7
suryasoma <suryasoma.9@gmail.com>
2023-05-02 14:15:42 -0700

    Creating BigQueryReadClient with UnboundedScheduledExecutorService (#951)
    * Creating BigQueryReadClient with UnboundedScheduledExecutorService
    
    * addressing review comments
    
    * renaming packages
    
    * refactor
    
    * updating bigquerystorage and addressing review comments
    
    * adding unit tests
    
    * refactor
    
    * refactor

--- bigquery-connector-common/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryClientFactory.java
+++ bigquery-connector-common/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryClientFactory.java
@@ -17,6 +17,7 @@ package com.google.cloud.bigquery.connector.common;
 
 import com.google.api.core.ApiFunction;
 import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.core.FixedExecutorProvider;
 import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
 import com.google.api.gax.rpc.HeaderProvider;
 import com.google.api.gax.rpc.UnaryCallSettings;
@@ -166,7 +167,9 @@ public class BigQueryClientFactory implements Serializable {
       BigQueryReadSettings.Builder clientSettings =
           BigQueryReadSettings.newBuilder()
               .setTransportChannelProvider(transportBuilder.build())
-              .setCredentialsProvider(FixedCredentialsProvider.create(credentials));
+              .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+              .setBackgroundExecutorProvider(
+                  FixedExecutorProvider.create(new UnboundedScheduledExecutorService()));
 
       bqConfig
           .getCreateReadSessionTimeoutInSeconds()
--- bigquery-connector-common/src/test/java/com/google/cloud/bigquery/connector/common/BigQueryClientFactoryTest.java
+++ bigquery-connector-common/src/test/java/com/google/cloud/bigquery/connector/common/BigQueryClientFactoryTest.java
@@ -15,6 +15,7 @@
  */
 package com.google.cloud.bigquery.connector.common;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
@@ -71,6 +72,17 @@ public class BigQueryClientFactoryTest {
   public BigQueryClientFactoryTest() {
     when(bigQueryConfig.useParentProjectForMetadataOperations()).thenReturn(false);
     this.headerProvider = HttpUtil.createHeaderProvider(bigQueryConfig, "test-agent");
+  }
+
+  @Test
+  public void testDefaultSettings() {
+    BigQueryClientFactory clientFactory =
+        new BigQueryClientFactory(bigQueryCredentialsSupplier, headerProvider, bigQueryConfig);
+    when(bigQueryConfig.getBigQueryProxyConfig()).thenReturn(bigQueryProxyConfig);
+    BigQueryReadClient readClient = clientFactory.getBigQueryReadClient();
+    assertNotNull(readClient);
+    assertThat(readClient.getSettings().getBackgroundExecutorProvider().getExecutor())
+        .isInstanceOf(UnboundedScheduledExecutorService.class);
   }
 
   @Test
