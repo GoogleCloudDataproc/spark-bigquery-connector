370e8be04ba589143c5f3414bfac6c58dbfc5e74
David Rabinowitz <davidrab@google.com>
2023-10-02 09:52:05 -0700

    Removing code duplication

---
+++ spark-bigquery-connector-common/src/main/java/com/google/cloud/spark/bigquery/direct/BigQueryRDDContext.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2022 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.spark.bigquery.direct;
+
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.connector.common.BigQueryClientFactory;
+import com.google.cloud.bigquery.connector.common.BigQueryStorageReadRowsTracer;
+import com.google.cloud.bigquery.connector.common.BigQueryTracerFactory;
+import com.google.cloud.bigquery.connector.common.BigQueryUtil;
+import com.google.cloud.bigquery.connector.common.ReadRowsHelper;
+import com.google.cloud.bigquery.storage.v1.DataFormat;
+import com.google.cloud.bigquery.storage.v1.ReadRowsRequest;
+import com.google.cloud.bigquery.storage.v1.ReadRowsResponse;
+import com.google.cloud.bigquery.storage.v1.ReadSession;
+import com.google.cloud.spark.bigquery.InternalRowIterator;
+import com.google.cloud.spark.bigquery.ReadRowsResponseToInternalRowIteratorConverter;
+import com.google.cloud.spark.bigquery.SchemaConverters;
+import com.google.cloud.spark.bigquery.SchemaConvertersConfiguration;
+import com.google.cloud.spark.bigquery.SparkBigQueryConfig;
+import com.google.cloud.spark.bigquery.metrics.SparkMetricsSource;
+import com.google.common.base.Joiner;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import org.apache.spark.InterruptibleIterator;
+import org.apache.spark.Partition;
+import org.apache.spark.SparkEnv;
+import org.apache.spark.TaskContext;
+import org.apache.spark.sql.catalyst.InternalRow;
+import org.apache.spark.sql.types.StructType;
+
+class BigQueryRDDContext {
+
+  private final Partition[] partitions;
+  private final ReadSession readSession;
+  private final String[] columnsInOrder;
+  private final Schema bqSchema;
+  private final SparkBigQueryConfig options;
+  private final BigQueryClientFactory bigQueryClientFactory;
+  private final BigQueryTracerFactory bigQueryTracerFactory;
+
+  private List<String> streamNames;
+
+  public BigQueryRDDContext(
+      Partition[] parts,
+      ReadSession readSession,
+      Schema bqSchema,
+      String[] columnsInOrder,
+      SparkBigQueryConfig options,
+      BigQueryClientFactory bigQueryClientFactory,
+      BigQueryTracerFactory bigQueryTracerFactory) {
+
+    this.partitions = parts;
+    this.readSession = readSession;
+    this.columnsInOrder = columnsInOrder;
+    this.bigQueryClientFactory = bigQueryClientFactory;
+    this.bigQueryTracerFactory = bigQueryTracerFactory;
+    this.options = options;
+    this.bqSchema = bqSchema;
+    this.streamNames = BigQueryUtil.getStreamNames(readSession);
+  }
+
+  public scala.collection.Iterator<InternalRow> compute(Partition split, TaskContext context) {
+    BigQueryPartition bigQueryPartition = (BigQueryPartition) split;
+    SparkMetricsSource sparkMetricsSource = new SparkMetricsSource();
+    SparkEnv.get().metricsSystem().registerSource(sparkMetricsSource);
+    BigQueryStorageReadRowsTracer tracer =
+        bigQueryTracerFactory.newReadRowsTracer(
+            Joiner.on(",").join(streamNames), sparkMetricsSource);
+
+    ReadRowsRequest.Builder request =
+        ReadRowsRequest.newBuilder().setReadStream(bigQueryPartition.getStream());
+
+    ReadRowsHelper readRowsHelper =
+        new ReadRowsHelper(
+            bigQueryClientFactory,
+            request,
+            options.toReadSessionCreatorConfig().toReadRowsHelperOptions(),
+            Optional.of(tracer));
+    Iterator<ReadRowsResponse> readRowsResponseIterator = readRowsHelper.readRows();
+
+    StructType schema =
+        options
+            .getSchema()
+            .orElse(
+                SchemaConverters.from(SchemaConvertersConfiguration.from(options))
+                    .toSpark(bqSchema));
+
+    ReadRowsResponseToInternalRowIteratorConverter converter;
+    if (options.getReadDataFormat().equals(DataFormat.AVRO)) {
+      converter =
+          ReadRowsResponseToInternalRowIteratorConverter.avro(
+              bqSchema,
+              Arrays.asList(columnsInOrder),
+              readSession.getAvroSchema().getSchema(),
+              Optional.of(schema),
+              Optional.of(tracer),
+              SchemaConvertersConfiguration.from(options));
+    } else {
+      converter =
+          ReadRowsResponseToInternalRowIteratorConverter.arrow(
+              Arrays.asList(columnsInOrder),
+              readSession.getArrowSchema().getSerializedSchema(),
+              Optional.of(schema),
+              Optional.of(tracer));
+    }
+
+    return new InterruptibleIterator<InternalRow>(
+        context,
+        new ScalaIterator<InternalRow>(
+            new InternalRowIterator(readRowsResponseIterator, converter, readRowsHelper, tracer)));
+  }
+
+  public Partition[] getPartitions() {
+    return partitions;
+  }
+}
--- spark-bigquery-scala-212-support/src/main/java/org/apache/spark/sql/PreScala213SparkSqlUtils.java
+++ spark-bigquery-scala-212-support/src/main/java/org/apache/spark/sql/PreScala213SparkSqlUtils.java
@@ -15,19 +15,17 @@
  */
 package org.apache.spark.sql;
 
+import com.google.cloud.spark.bigquery.SparkBigQueryUtil;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.catalyst.analysis.SimpleAnalyzer$;
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder;
 import org.apache.spark.sql.catalyst.encoders.RowEncoder;
 import org.apache.spark.sql.catalyst.expressions.Attribute;
 import org.apache.spark.sql.catalyst.expressions.AttributeReference;
-import org.apache.spark.sql.catalyst.expressions.NamedExpression;
 import org.apache.spark.sql.types.StructType;
 import scala.collection.JavaConverters;
-import scala.collection.mutable.ListBuffer;
 
 public class PreScala213SparkSqlUtils extends SparkSqlUtils {
 
@@ -60,18 +58,7 @@ public class PreScala213SparkSqlUtils extends SparkSqlUtils {
   // `toAttributes` is protected[sql] starting spark 3.2.0, so we need this call to be in the same
   // package. Since Scala 2.13/Spark 3.3 forbids it, the implementation has been ported to Java
   public static scala.collection.Seq<AttributeReference> toAttributes(StructType schema) {
-    List<AttributeReference> result =
-        Stream.of(schema.fields())
-            .map(
-                field ->
-                    new AttributeReference(
-                        field.name(),
-                        field.dataType(),
-                        field.nullable(),
-                        field.metadata(),
-                        NamedExpression.newExprId(),
-                        new ListBuffer<String>().toSeq()))
-            .collect(Collectors.toList());
-    return JavaConverters.asScalaBuffer(result).toSeq();
+    return JavaConverters.asScalaBuffer(SparkBigQueryUtil.schemaToAttributeReferences(schema))
+        .toSeq();
   }
 }
