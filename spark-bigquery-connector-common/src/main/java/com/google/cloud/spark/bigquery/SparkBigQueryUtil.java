73a21f4f81d10a9d223aa57a56bc5445b27d9b4d
Thomas Powell <thomas.samuel.powell@gmail.com>
2024-06-07 13:11:05 +0100

    Allow GCS bucket to be supplied including a scheme.

--- spark-bigquery-connector-common/src/main/java/com/google/cloud/spark/bigquery/SparkBigQueryUtil.java
+++ spark-bigquery-connector-common/src/main/java/com/google/cloud/spark/bigquery/SparkBigQueryUtil.java
@@ -41,6 +41,8 @@ import java.util.ServiceLoader;
 import java.util.UUID;
 import java.util.stream.Stream;
 import javax.validation.constraints.NotNull;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -116,8 +118,8 @@ public class SparkBigQueryUtil {
         gcsPath =
             new Path(
                 String.format(
-                    "gs://%s/%s",
-                    config.getPersistentGcsBucket().get(), config.getPersistentGcsPath().get()));
+                    "%s/%s",
+                    getBucketAndScheme(config.getPersistentGcsBucket().get()), config.getPersistentGcsPath().get()));
         FileSystem fs = gcsPath.getFileSystem(conf);
         if (fs.exists(gcsPath)) {
           throw new IllegalArgumentException(
@@ -140,16 +142,21 @@ public class SparkBigQueryUtil {
       throws IOException {
     boolean needNewPath = true;
     Path gcsPath = null;
+    String gcsBucketAndScheme = getBucketAndScheme(gcsBucket);
     while (needNewPath) {
       gcsPath =
           new Path(
               String.format(
-                  "gs://%s/.spark-bigquery-%s-%s", gcsBucket, applicationId, UUID.randomUUID()));
+                  "%s/.spark-bigquery-%s-%s", gcsBucketAndScheme, applicationId, UUID.randomUUID()));
       FileSystem fs = gcsPath.getFileSystem(conf);
       needNewPath = fs.exists(gcsPath);
     }
 
     return gcsPath;
+  }
+
+  private static String getBucketAndScheme(String gcsBucket) {
+    return gcsBucket.contains("://") ? gcsBucket : "gs://" + gcsBucket;
   }
 
   public static String getJobId(SQLConf sqlConf) {
