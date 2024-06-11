2c1a8650968593e0b44c3e229e8d659768ff6ff0
David Rabinowitz <davidrabinowitz@users.noreply.github.com>
2024-06-11 10:15:43 -0700

    Issue #1223: gRPC status creates 'object not serializable' errors (#1240)

--- bigquery-connector-common/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryConnectorException.java
+++ bigquery-connector-common/src/main/java/com/google/cloud/bigquery/connector/common/BigQueryConnectorException.java
@@ -18,12 +18,23 @@ package com.google.cloud.bigquery.connector.common;
 import static com.google.cloud.bigquery.connector.common.BigQueryErrorCode.BIGQUERY_INVALID_SCHEMA;
 import static com.google.cloud.bigquery.connector.common.BigQueryErrorCode.UNKNOWN;
 
+import io.grpc.StatusException;
+import io.grpc.StatusRuntimeException;
+
 public class BigQueryConnectorException extends RuntimeException {
 
   final BigQueryErrorCode errorCode;
 
   public BigQueryConnectorException(String message) {
     this(UNKNOWN, message);
+  }
+
+  public BigQueryConnectorException(String message, StatusException cause) {
+    this(message, new SerializableStatusException(cause));
+  }
+
+  public BigQueryConnectorException(String message, StatusRuntimeException cause) {
+    this(message, new SerializableStatusException(cause));
   }
 
   public BigQueryConnectorException(String message, Throwable cause) {
@@ -33,6 +44,16 @@ public class BigQueryConnectorException extends RuntimeException {
   public BigQueryConnectorException(BigQueryErrorCode errorCode, String message) {
     super(message);
     this.errorCode = errorCode;
+  }
+
+  public BigQueryConnectorException(
+      BigQueryErrorCode errorCode, String message, StatusException cause) {
+    this(errorCode, message, new SerializableStatusException(cause));
+  }
+
+  public BigQueryConnectorException(
+      BigQueryErrorCode errorCode, String message, StatusRuntimeException cause) {
+    this(errorCode, message, new SerializableStatusException(cause));
   }
 
   public BigQueryConnectorException(BigQueryErrorCode errorCode, String message, Throwable cause) {
@@ -52,6 +73,43 @@ public class BigQueryConnectorException extends RuntimeException {
 
     public InvalidSchemaException(String message, Throwable t) {
       super(BIGQUERY_INVALID_SCHEMA, message, t);
+    }
+  }
+
+  /**
+   * StatusRuntimeException contains non-serializable members causing issues when sending exceptions
+   * from the executors to the driver. this class provides a serializable alternative.
+   */
+  public static class SerializableStatusException extends RuntimeException {
+    private final String wrappedExceptionClassName;
+    private final String message;
+    private final StackTraceElement[] stackTrace;
+
+    SerializableStatusException(StatusException wrapped) {
+      this.wrappedExceptionClassName = wrapped.getClass().getName();
+      this.message = wrapped.getMessage();
+      this.stackTrace = wrapped.getStackTrace().clone();
+    }
+
+    SerializableStatusException(StatusRuntimeException wrapped) {
+      this.wrappedExceptionClassName = wrapped.getClass().getName();
+      this.message = wrapped.getMessage();
+      this.stackTrace = wrapped.getStackTrace().clone();
+    }
+
+    @Override
+    public String getMessage() {
+      return this.message;
+    }
+
+    @Override
+    public String toString() {
+      return this.wrappedExceptionClassName + ": " + this.message;
+    }
+
+    @Override
+    public StackTraceElement[] getStackTrace() {
+      return this.stackTrace;
     }
   }
 }
