31fb3f3fabe1a074f38c8ddd74c2c50b3ea7b0fb
David Rabinowitz <davidrab@google.com>
2024-06-10 19:05:22 -0700

    Issue #1223: gRPC status creates 'object not serializable' errors

---
+++ bigquery-connector-common/src/test/java/com/google/cloud/bigquery/connector/common/BigQueryConnectorExceptionTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigquery.connector.common;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.grpc.Status;
+import io.grpc.StatusException;
+import io.grpc.StatusRuntimeException;
+import org.junit.Test;
+
+public class BigQueryConnectorExceptionTest {
+
+  @Test
+  public void testStatusExceptionSerialization() {
+    StatusException ex = new StatusException(Status.CANCELLED);
+    BigQueryConnectorException original =
+        new BigQueryConnectorException(BigQueryErrorCode.UNKNOWN, "testing", ex);
+    BigQueryConnectorException copied = BigQueryUtil.verifySerialization(original);
+    assertThat(copied.getMessage()).isEqualTo("testing");
+    assertThat(copied.getCause().toString()).contains("StatusException");
+  }
+
+  @Test
+  public void testStatusRuntimeExceptionSerialization() {
+    StatusRuntimeException ex = new StatusRuntimeException(Status.CANCELLED);
+    BigQueryConnectorException original =
+        new BigQueryConnectorException(BigQueryErrorCode.UNKNOWN, "testing", ex);
+    BigQueryConnectorException copied = BigQueryUtil.verifySerialization(original);
+    assertThat(copied.getMessage()).isEqualTo("testing");
+    assertThat(copied.getCause().toString()).contains("StatusRuntimeException");
+  }
+}
