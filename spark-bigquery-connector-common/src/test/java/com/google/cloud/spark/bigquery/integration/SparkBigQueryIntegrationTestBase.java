f6ab15425fbe78496ab7fffb3750cfa41954fd77
David Rabinowitz <davidrabinowitz@users.noreply.github.com>
2025-06-02 21:37:48 -0700

    Creating unique dataset name per test name (#1375)

--- spark-bigquery-connector-common/src/test/java/com/google/cloud/spark/bigquery/integration/SparkBigQueryIntegrationTestBase.java
+++ spark-bigquery-connector-common/src/test/java/com/google/cloud/spark/bigquery/integration/SparkBigQueryIntegrationTestBase.java
@@ -52,46 +52,4 @@ public class SparkBigQueryIntegrationTestBase {
       spark.sparkContext().setLogLevel("WARN");
     }
   }
-
-  protected static class TestDataset extends ExternalResource {
-
-    String testDataset =
-        String.format("spark_bigquery_%d_%d", System.currentTimeMillis(), System.nanoTime());
-
-    @Override
-    protected void before() throws Throwable {
-      IntegrationTestUtils.createDataset(testDataset);
-      IntegrationTestUtils.runQuery(
-          String.format(
-              TestConstants.ALL_TYPES_TABLE_QUERY_TEMPLATE,
-              testDataset,
-              TestConstants.ALL_TYPES_TABLE_NAME));
-      IntegrationTestUtils.createView(testDataset, TestConstants.SHAKESPEARE_VIEW);
-      IntegrationTestUtils.runQuery(
-          String.format(
-              TestConstants.STRUCT_COLUMN_ORDER_TEST_TABLE_QUERY_TEMPLATE,
-              testDataset,
-              TestConstants.STRUCT_COLUMN_ORDER_TEST_TABLE_NAME));
-      IntegrationTestUtils.runQuery(
-          String.format(
-              TestConstants.DIFF_IN_SCHEMA_SRC_TABLE,
-              testDataset,
-              TestConstants.DIFF_IN_SCHEMA_SRC_TABLE_NAME));
-      IntegrationTestUtils.runQuery(
-          String.format(
-              TestConstants.DIFF_IN_SCHEMA_SRC_TABLE_WITH_DESCRIPTION,
-              testDataset,
-              TestConstants.DIFF_IN_SCHEMA_SRC_TABLE_NAME_WITH_DESCRIPTION));
-    }
-
-    @Override
-    protected void after() {
-      IntegrationTestUtils.deleteDatasetAndTables(testDataset);
-    }
-
-    @Override
-    public String toString() {
-      return testDataset;
-    }
-  }
 }
